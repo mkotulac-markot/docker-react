53651cc50cc55f8f1fd771c2228094e787db4a9a
mkotulac <m.kotulac@sap.com>
2023-12-20 15:38:44 +0100

    add feature branch

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there!!!aaaa
+          Add feature branch
         </p>
         <a
           className="App-link"
